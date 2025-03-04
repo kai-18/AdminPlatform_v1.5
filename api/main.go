fe6223445f1ecdf92758e6f752a3dd9a913c3a5f
Kai <kai-18>
2025-03-04 18:24:38 +0100

    updates

--- api/main.go
+++ api/main.go
@@ -9,7 +9,7 @@ import (
 
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
-	_ "github.com/go-sql-driver/mysql" // Add this import for MySQL driver
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/handlers"
 )
 
